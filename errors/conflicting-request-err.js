2ea0ed9387bc6b729dbe51d5b4bd9982037dd0d8
xloppi <hlopkovalesha@yandex.ru>
2022-02-08 17:19:29 +0300

    add errors: 400, 409; add error handler, auth

---
+++ errors/conflicting-request-err.js
@@ -0,0 +1,9 @@
+
+class ConflictingError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+  }
+}
+
+module.exports = ConflictingError;
---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/auth-err');
+
+const { JWT_SECRET_KEY } = require('../utils/conf');
+
+const { AUTH_ERR } = require('../utils/const_messages');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError(AUTH_ERR));
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET_KEY);
+  } catch (err) {
+    return next(new AuthError(AUTH_ERR));
+  }
+
+  req.user = payload;
+
+  return next();
+};
